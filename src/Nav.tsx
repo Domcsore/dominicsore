8c1879df584e78337384697646d6871a03ce0510
Dominic Sore <domcsore@googlemail.com>
2022-10-28 16:46:42 +0100

    landing page

---
+++ src/Nav.tsx
@@ -0,0 +1,7 @@
+import "./Nav.css";
+
+const Nav: React.FC = () => {
+  return <nav className="main">NAV</nav>;
+};
+
+export default Nav;
---
+++ src/sections/Landing.tsx
@@ -0,0 +1,14 @@
+import "./Landing.css";
+
+const Landing: React.FC = () => {
+  return (
+    <div id="home-section" className="section full-screen">
+      <div className="landing-image" />
+      <div id="landing-title-container">
+        <h1 id="landing-title">DOMINIC SORE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
